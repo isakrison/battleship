f4fe5b355ebae16d97a4546834b2a327f0603bf8
Ingrid Sakrison <Ingrid Sakrison>
2016-08-28 22:26:11 -0500

    ~ Implemented ship placement ~ Got rid of HTML monitor; wrote a console log function instead ~ Quality of life stuff in other files for debugging ~ Added notes

--- NW_Battleship_challenge/CandidateAi.js
+++ NW_Battleship_challenge/CandidateAi.js
@@ -28,42 +28,30 @@ CandidateAI.prototype.initializeGame = function() {
 		clear : 8
 	}
 	
-	monitor = document.createElement("table");
-	monitor.id = "monitor";
-	monitor.style = "border:solid;border-color:black;border-width:2px;";
-	var tableRow, tableCell, status;
-	cells = [];
+	cells = [];	
+	
+	// initialize cells and set initial status
 	for (i = 0; i < 10; i++) {
-		cells[i] = [];
-		tableRow = document.createElement("tr");
-		monitor.appendChild(tableRow);
-		
-		for (j = 0; j < 10; j++) {
-			
-			tableCell = document.createElement("td");
-			tableCell.id = "row" + i + "_column" + j;
-			tableCell.style = "height:30px;width:30px;background-color:gray;";
-			status = cellStatus.unknown;
+		cells[i] = [];		
+		for (j = 0; j < 10; j++) {			
+
+			status = cellStatus.unknown;			
 
 			if ((i + j) % 4 == 0) {
 				status = cellStatus.primaryTarget;
-				tableCell.style.backgroundColor = "blue";
 			}else{			
 				if ((i + j) % 2 == 0) {
 					status = cellStatus.secondaryTarget;
-					tableCell.style.backgroundColor = "lightblue";
 				}
 			}
-			tableRow.appendChild(tableCell);
 			
 			cells[i].push({
-				row : i,
-				column : j,
+				x : i,
+				y : j,
 				status : status
 			});
 		}
 	}
-	document.body.appendChild(monitor);
 	
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~
     var x = -1, y = 0;
@@ -78,8 +66,86 @@ CandidateAI.prototype.initializeGame = function() {
         };
     }
 	
-	this.getRandomInt = function (min, max) {
-		return Math.floor(Math.random() * (max - min)) + min;
+	this.getRandomInt = function (min, max) {	
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	
+	// shamelessly stolen from battleship.js
+	this.outputGameStatus = function() {
+		
+		var shipTable = "";
+		shipTable += "    0  1  2  3  4  5  6  7  8  9\n\n";
+		for (var row = 0; row < 10; row++) {
+			shipTable += row + "  ";
+			for (var column = 0; column < 10; column++) {
+				var ship = this.player.grid.getShipByCoord(row, column);
+				var shipType = ship ? ship.type : "";
+				switch (shipType) {
+					case Fleet.BATTLESHIP:
+						shipTable += " B ";
+						break;
+					case Fleet.CARRIER:
+						shipTable += " C ";
+						break;
+					case Fleet.PATROLBOAT:
+						shipTable += " P ";
+						break;
+					case Fleet.DESTROYER:
+						shipTable += " D ";
+						break;
+					case Fleet.SUBMARINE:
+						shipTable += " S ";
+						break;
+					default:
+						shipTable += " - ";
+						break;
+				}
+			}
+			shipTable += "\n";
+		}
+		shipTable += "\n";
+		
+		var shotTable = "";
+		shotTable += "    0  1  2  3  4  5  6  7  8  9\n\n";
+		for (var row = 0; row < 10; row++) {
+			shotTable += row + "  ";
+			for (var column = 0; column < 10; column++) {
+				var cell = cells[column][row];
+				switch (parseInt(cell.status)) {
+					case cellStatus.primaryTarget:
+						shotTable += " 1 ";
+						break;
+					case cellStatus.secondaryTarget:
+						shotTable += " 2 ";
+						break;
+					case cellStatus.possibleShip:
+						shotTable += " P ";
+						break;
+					case cellStatus.hit:
+						shotTable += " X ";
+						break;
+					case cellStatus.miss:
+						shotTable += " 0 ";
+						break;
+					case cellStatus.sunk:
+						shotTable += " S ";
+						break;
+					case cellStatus.unknown:
+						shotTable += " ? ";
+						break;
+					default:
+						shotTable += " - ";
+						break;
+				}
+			}
+			shotTable += "\n";
+		}
+		shotTable += "\n";
+		
+		console.log("Upper grid: \n");
+		console.log(shotTable);
+		console.log("Lower grid: \n");
+		console.log(shipTable);
 	}
 };
 
@@ -87,11 +153,85 @@ CandidateAI.prototype.initializeGame = function() {
  * [called before each game. This is where you must place your ships.]
  */
 CandidateAI.prototype.startGame = function() {
-    this.player.grid.dockShip(0, 0, Ship.VERTICAL, Fleet.CARRIER);
-    this.player.grid.dockShip(1, 0, Ship.VERTICAL, Fleet.BATTLESHIP);
-    this.player.grid.dockShip(2, 0, Ship.VERTICAL, Fleet.DESTROYER);
-    this.player.grid.dockShip(3, 0, Ship.VERTICAL, Fleet.SUBMARINE);
-    this.player.grid.dockShip(4, 0, Ship.VERTICAL, Fleet.PATROLBOAT);
+	var x, y, orientation;
+	var tableCell;
+	
+	// dock Carrier
+	do{
+		orientation = this.getRandomInt(0, 1) == 0 ? Ship.HORIZONTAL : Ship.VERTICAL;
+		if (orientation == Ship.HORIZONTAL) {
+			x = this.getRandomInt(0, 5);
+			y = this.getRandomInt(0, 9);			
+		} else {
+			x = this.getRandomInt(0, 9);
+			y = this.getRandomInt(0, 5);
+		}
+	}
+	while (! this.player.grid.dockShip(x, y, orientation, Fleet.CARRIER));	
+
+	orientation = this.getRandomInt(0, 1) == 0 ? Ship.HORIZONTAL : Ship.VERTICAL;
+	if (orientation == Ship.HORIZONTAL) {
+		x = this.getRandomInt(0, 5);
+		y = this.getRandomInt(0, 9);			
+	} else {
+		x = this.getRandomInt(0, 9);
+		y = this.getRandomInt(0, 5);
+	}
+	this.player.grid.dockShip(x, y, orientation, Fleet.CARRIER);
+	
+	// dock Battleship
+	do{
+		orientation = this.getRandomInt(0, 1) == 0 ? Ship.HORIZONTAL : Ship.VERTICAL;
+		if (orientation == Ship.HORIZONTAL) {
+			x = this.getRandomInt(0, 6);
+			y = this.getRandomInt(0, 9);			
+		} else {
+			x = this.getRandomInt(0, 9);
+			y = this.getRandomInt(0, 6);
+		}
+	}
+	while (! this.player.grid.dockShip(x, y, orientation, Fleet.BATTLESHIP));
+
+	// dock Destroyer
+	do{
+		orientation = this.getRandomInt(0, 1) == 0 ? Ship.HORIZONTAL : Ship.VERTICAL;
+		if (orientation == Ship.HORIZONTAL) {
+			x = this.getRandomInt(0, 7);
+			y = this.getRandomInt(0, 9);			
+		} else {
+			x = this.getRandomInt(0, 9);
+			y = this.getRandomInt(0, 7);
+		}
+	}
+	while (! this.player.grid.dockShip(x, y, orientation, Fleet.DESTROYER));
+	
+	// dock Submarine
+	do{
+		orientation = this.getRandomInt(0, 1) == 0 ? Ship.HORIZONTAL : Ship.VERTICAL;
+		if (orientation == Ship.HORIZONTAL) {
+			x = this.getRandomInt(0, 7);
+			y = this.getRandomInt(0, 9);			
+		} else {
+			x = this.getRandomInt(0, 9);
+			y = this.getRandomInt(0, 7);
+		}
+	}
+	while (! this.player.grid.dockShip(x, y, orientation, Fleet.SUBMARINE));
+	
+	// dock Patrolboat
+	do{
+		orientation = this.getRandomInt(0, 1) == 0 ? Ship.HORIZONTAL : Ship.VERTICAL;
+		if (orientation == Ship.HORIZONTAL) {
+			x = this.getRandomInt(0, 8);
+			y = this.getRandomInt(0, 9);			
+		} else {
+			x = this.getRandomInt(0, 9);
+			y = this.getRandomInt(0, 8);
+		}
+	}
+	while (! this.player.grid.dockShip(x, y, orientation, Fleet.PATROLBOAT));
+	
+	this.outputGameStatus();
 };
 
 /**
@@ -99,7 +239,6 @@ CandidateAI.prototype.startGame = function() {
  */
 CandidateAI.prototype.shoot = function() {
     var coords = this.getNextCoord();
-	alert("cells[" + coords.y + "][" + coords.x + "].status = " + cells[coords.y][coords.x].status);
     var result = this.player.shoot(coords.x, coords.y);
     //result is one of Cell.<type> so that you can re-shoot if necessary. (e.g. you are shooting someplace you already shot)
 };
