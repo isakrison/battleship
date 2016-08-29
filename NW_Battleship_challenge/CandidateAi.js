d8f5573e593fcec36af033c1720b5f1d8003acaa
Ingrid Sakrison <solafide134@gmail.com>
2016-08-29 17:31:20 -0500

    Added function to get all possible locations for enemy ships

--- NW_Battleship_challenge/CandidateAi.js
+++ NW_Battleship_challenge/CandidateAi.js
@@ -28,7 +28,8 @@ CandidateAI.prototype.initializeGame = function() {
 		clear : 8
 	}
 	
-	cells = [];	
+	cells = [];
+	enemyShips = [];
 	
 	// initialize cells and set initial status
 	for (i = 0; i < 10; i++) {
@@ -48,31 +49,136 @@ CandidateAI.prototype.initializeGame = function() {
 			cells[i].push({
 				x : i,
 				y : j,
-				status : status
+				status : status,
+				weight : 0
 			});
 		}
 	}
 	
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~
-    var x = -1, y = 0;
+	// initialize enemy ships for tracking status
+	enemyShips.push({
+		type : "carrier",
+		size : 5,
+		possibleLocations : 0,
+		sunk : false
+	});	
+	enemyShips.push({
+		type : "battleship",
+		size : 4,
+		possibleLocations : 0,
+		sunk : false
+	});	
+	enemyShips.push({
+		type : "destroyer",
+		size : 3,
+		possibleLocations : 0,
+		sunk : false
+	});	
+	enemyShips.push({
+		type : "submarine",
+		size : 3,
+		possibleLocations : 0,
+		sunk : false
+	});	
+	enemyShips.push({
+		type : "patrolboat",
+		size : 2,
+		possibleLocations : 0,
+		sunk : false
+	});
+
+	// ~~~~~~~~~~~~~~~~~~~~~
+	// function getNextCoord
+	// ~~~~~~~~~~~~~~~~~~~~~
+	x = -1;
+	y = -1;
     this.getNextCoord = function() {
-        if (++x > 9) {
-            x = 0;
-            ++y;
-        }
-        return {
-            x: x,
-            y: y
-        };
-    }
 	
+		// or, instead of this mess, we could get probabilities for all primary and secondary target cells, and add 1 to primaries to weight them,
+		// depending on what ships haven't been sunk yet
+		
+		this.getPossibleShipLocations();
+		
+		alert("Possible Carrier locations: " + enemyShips[0].possibleLocations);
+
+		
+			
+		// placehoder code:	
+		if (x == 9) {
+			x = -1;
+		}
+		if (y == 9) {
+			y = -1;
+		}
+		x++;
+		y++;
+		return ({x:x, y:y});
+		// end placeholder
+		
+		// we'd handle hits and the cells surrounding them differently (or just assign them probability + 2)
+			// or or... do the probability math for cells surrounding a hit, and just let the chips fall where they may? 
+				// depends on what the actual probability turns out to be, I think
+				// ...this is turning out not to be a small project :P
+    } // end function getNextCoord
+	
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// function getPossibleShipLocations
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	this.getPossibleShipLocations = function () {
+		// get total possible locations remaining for each unsunk enemy ship
+		var ship;
+		var maxStartingX, maxStartingY;
+		var possibleLocation;		
+		for (n = 0; n < enemyShips.length; n++) {
+			ship = enemyShips[n];
+			if (!ship.sunk) {
+				ship.possibleLocations = 0;
+				maxStartingX = cells.length - ship.size + 1
+				for (i = 0; i < maxStartingX; i++) {
+					maxStartingY = cells[i].length - ship.size + 1;
+					for (j = 0; j < maxStartingY; j++) {
+						// check horizontal ship position at this cell
+						possibleLocation = true;
+						for (p = 0; p < ship.size; p++) {
+							var currentStatus = cells[i+p][j].status;
+							if (currentStatus == cellStatus.miss || currentStatus == cellStatus.sunk || currentStatus == cellStatus.clear) {
+								possibleLocation = false;
+								break;
+							}
+						}
+						if (possibleLocation) {
+							ship.possibleLocations++;
+						}
+						// check vertical ship position at this cell
+						possibleLocation = true;
+						for (p = 0; p < ship.size; p++) {
+							var currentStatus = cells[i][j+p].status;
+							if (currentStatus == cellStatus.miss || currentStatus == cellStatus.sunk || currentStatus == cellStatus.clear) {
+								possibleLocation = false;
+								break;
+							}
+						}
+						if (possibleLocation) {
+							ship.possibleLocations++;
+						}
+					}
+				}
+			}
+		}	
+	}	
+	
+	// ~~~~~~~~~~~~~~~~~~~~~
+	// function getRandomInt
+	// ~~~~~~~~~~~~~~~~~~~~~
 	this.getRandomInt = function (min, max) {	
 		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
+	} // end function getRandomInt
 	
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~
+	// function outputGameStatus
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~
 	// shamelessly stolen from battleship.js
-	this.outputGameStatus = function() {
-		
+	this.outputGameStatus = function () {		
 		var shipTable = "";
 		shipTable += "    0  1  2  3  4  5  6  7  8  9\n\n";
 		for (var row = 0; row < 10; row++) {
@@ -146,7 +252,7 @@ CandidateAI.prototype.initializeGame = function() {
 		console.log(shotTable);
 		console.log("Lower grid: \n");
 		console.log(shipTable);
-	}
+	} // end function outputGameStatus
 };
 
 /**
@@ -238,6 +344,7 @@ CandidateAI.prototype.startGame = function() {
  * [called each time it is your turn to shoot]
  */
 CandidateAI.prototype.shoot = function() {
+	alert("got here: shoot");
     var coords = this.getNextCoord();
     var result = this.player.shoot(coords.x, coords.y);
     //result is one of Cell.<type> so that you can re-shoot if necessary. (e.g. you are shooting someplace you already shot)
